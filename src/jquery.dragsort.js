96c62ed7a33995ee3914056c126a237a3ecc1fe2
nickbuddendotcom <hello@nickbudden.com>
2015-05-11 17:21:06 +0800

    Nesting support

--- src/jquery.dragsort.js
+++ src/jquery.dragsort.js
@@ -10,6 +10,7 @@
     minSelection  : Number.POSITIVE_INFINITY,
     animationSpeed: 150,
     boundingBoxPadding: 75,
+    nestedMenuClass: 'nested_menu',
     multi: true,
     onSortStart: function() {},
     onSort: function() {},
@@ -18,8 +19,31 @@
   };
 
   var $window = $(window);
+  var stopProp = {};
   var $dragPreview;
   var dropIndex;
+  var userSelectCSS;
+
+
+
+
+  // - Meta clicks on parents select or deselect the whole children, depending whether all children are already selected...
+  // - How do I get my nesting only going to the depth I want?
+  // - remove nested list from preview
+  // - I need filters for locked and unlocked too...could want to have filters for lots of things...
+
+  // - Refactor and comment...
+  // - do really fast drags work?
+  // - indexes API...make sure this can account for dragging something things that come from children and some things that come from parents at the same time...ie meta clicking something in a child and something in parents
+  // - After I fix that it's time to get my indexes API working, and rework this
+  // - back into Vectr w/ the modified API
+  // - I'm gonna need list a nesting rule...a callback that defines the nesting rule...some things can nest deep
+  //   and others can't
+
+
+
+
+
 
   /**
    * Plugin contructor. Note that this.$element must have position:relative or
@@ -29,8 +53,9 @@
    * @param {object} options Plugin options
    */
   function Plugin( element, options ) {
-    this.element = element;
-    this.$element = $(element);
+    this.element      = element;
+    this.$element     = $(element);
+    this.$activeList  = this.$element;
 
     this.options = $.extend({}, defaults, options) ;
 
@@ -62,11 +87,11 @@
    * @return {undefined}
    */
   Plugin.prototype.sortStart = function(e) {
-    var self = this;
+    var self      = this;
+    var mockEvent = $.Event('', { currentTarget: self.$activeItem[0] });
 
     self.itemWidth    = self.$activeItem.outerWidth();
     self.itemHeight   = self.$activeItem.outerHeight();
-    self.$items       = self.$element.find(self.options.sortSelector + ':not(.' + self.options.selectedClass + ')');
     self.centerPoints = [];
     $dragPreview      = self.$activeItem.clone();
     dropIndex         = 0;
@@ -87,20 +112,27 @@
       $dragPreview.append( $('<div class="drag_preview_number" />').text(self.$sortSelection.length) );
     }
 
-    // Set draggable item center points
-    self.setCenterPoints();
+    // Set the active list and items
+    self.setActiveList( mockEvent );
 
     // Hide the selected pages & Set Center Points.
     self.$sortSelection.slideUp(self.options.animationSpeed, function() {
       // Reset draggable item center points once everything's scrolled up
-      self.setCenterPoints();
+      self.setActiveList( mockEvent );
       self.setDragBounds();
     });
+
+    // Save the current user select, so that it can be replaced
+    // when the drag ends. Then disable select while dragging
+    userSelectCSS = self.$element.css('user-select');
+    self.$element.css('user-select', 'none');
 
     // position the drag sort initially
     self.sort.call(self, e);
 
-    $window.on('mousemove', $.proxy(self.sort, self));
+    self.$element.on('mouseenter mouseleave', '.' + self.options.nestedMenuClass, $.proxy(self.setActiveList, self));
+
+    $window.on('mousemove mousewheel', $.proxy(self.sort, self));
     $window.one('mouseup', $.proxy(self.sortEnd, self));
 
     // Callback
@@ -131,6 +163,7 @@
     var self = this;
 
     // Position the preview
+    // TODO: Take into account window scroll...
     $dragPreview.css({
       left: e.clientX - self.itemWidth / 2,
       top: e.clientY - self.itemHeight / 2
@@ -156,10 +189,7 @@
     }
 
     // Get cursor's y-position relative to the scrolled element
-    var cursorY = e.pageY - self.$element.offset().top + self.$element.scrollTop();
-
-    // ok are centerpoints wrong or is cursorY wrong...centerpoints are wrong...
-    // or aren't they suppose to be relative to window?
+    var cursorY = e.pageY - self.$activeList.offset().top + self.$activeList.scrollTop();
 
     // See which center point this cursor comes after
     var insertAfter = self.centerPoints.reduce(function(prev, curr) {
@@ -172,21 +202,25 @@
     // Change the position of the drop preview if we've dragged to a
     // new position
     if(dropIndex !== pointIndex) {
+      var $preview = $('<div class="drop_preview" />').hide();
+
       self.clearDropPreviews();
       self.centerPoints = [];
 
       // Append after indexed element
       if( self.$items.eq(pointIndex).length !== 0 ) {
-        $('<div class="drop_preview" />').hide().insertBefore( self.$items.eq(pointIndex) ).slideDown(self.options.animationSpeed, function() {
-          self.setCenterPoints();
+        $preview.insertBefore( self.$items.eq(pointIndex) ).slideDown(self.options.animationSpeed, function() {
+          self.setCenterPoints(e);
         });
 
       // Append to end of container
       } else {
-        $('<div class="drop_preview" />').hide().appendTo( self.$element ).slideDown(self.options.animationSpeed, function() {
-          self.setCenterPoints();
+        $preview.appendTo( self.$activeList ).slideDown(self.options.animationSpeed, function() {
+          self.setCenterPoints(e);
         });
 
+        // Make sure the drop preview is scrolled into view
+        self.$activeList.scrollTop( self.$activeList.outerHeight() );
       }
 
       dropIndex = pointIndex;
@@ -205,8 +239,10 @@
    */
   Plugin.prototype.sortEnd = function(e) {
     this.clearDropPreviews();
+
     $dragPreview.remove();
-    $window.off('mousemove', this.sort);
+    $window.off('mousemove mousewheel', this.sort);
+    this.$element.off('mouseenter mouseleave', '.' + this.options.nestedMenuClass, this.setActiveList);
 
     // If droppped outside the container cancel the sort
     if(!this.isWithinDragBounds(e)) {
@@ -231,6 +267,9 @@
       this.$sortSelection.appendTo( this.$element ).slideDown(this.options.animationSpeed);
     }
 
+    // Replace the original user-select
+    this.$element.css('user-select', userSelectCSS);
+
     // Callback
     this.options.onSortEnd.call(this, e, dropIndex, originalIndexes);
   };
@@ -244,9 +283,10 @@
    * @return {undefined}
    */
   Plugin.prototype.mousedownItem = function(e) {
+    var ig = this.options.ignore;
 
-    // If right click, this event happens on ignored classes, ignore it
-    if( e.which === 3 || this.options.ignore && ($(e.target).is(this.options.ignore) || $(e.target).parents(this.options.ignore).length > 0) ) {
+    // If already did a mousedown, right click, or this event happens on ignored classes, ignore it
+    if( isBubble('mousedown') || e.which === 3 || ig && ($(e.target).is(ig) || $(e.target).parents(ig).length > 0) ) {
       return;
     }
 
@@ -273,7 +313,7 @@
      * @param  {object} e jQuery Event Object
      * @return {undefined}
      */
-     function detectDrag(e) {
+    function detectDrag(e) {
       if(Math.abs(ox - e.pageX) < 20 && Math.abs(oy - e.pageY) < 20) {
         return;
       }
@@ -304,14 +344,21 @@
    * @return {undefined}
    */
   Plugin.prototype.clickItem = function(e) {
-    var $clicked    = $(e.target);
-    var clickIndex  = $clicked.index();
+
+    // Only handle on first clicked
+    if(isBubble('click')){
+      return;
+    }
+
+    var self        = this;
+    var $clicked    = $(e.currentTarget);
+    var ss          = self.options.sortSelector;
+    var clickIndex  = $clicked.index(ss);
 
     // If meta or control click, add or remove clicked item from selection
     if(this.options.multi && e.metaKey || e.ctrlKey) {
-
       if($clicked.hasClass(this.options.selectedClass)) {
-        this.deselectItems( $clicked );
+        this.deselectItems( $clicked.find(ss).add($clicked) );
 
         // Remove from shiftRange, if necessary
         if(this.$shiftRange) {
@@ -323,32 +370,63 @@
           this.shiftIndex = 0;
         }
 
+        // Deselect parent. Parent can only be selected if ALL children are selected.
+        this.deselectItems( $clicked.parents(ss+'.'+self.options.selectedClass) );
+
       } else {
-        this.selectItems( $clicked );
+        this.selectItems( $clicked.find(ss).add($clicked) );
         this.shiftIndex = clickIndex;
       }
     }
 
+    // TODO: Some of shift select still doesn't work perfectly but good enough
+    // for now. It doesn't keep things outside the shiftRange selected, and can
+    // sometimes have parent-level items deselected when not all children are selected
+
     // If shift click, set (or reset) range of selection
     if(this.options.multi && e.shiftKey) {
-
-      var prevSelector = this.options.sortSelector + ':eq(' + this.shiftIndex + ')';
       var $newShiftRange;
+      var $commonParent;
 
+      // Set to only the clicked element
       if(this.shiftIndex === clickIndex) {
         $newShiftRange = $clicked;
-      } else if(this.shiftIndex < clickIndex) {
-        $newShiftRange = $clicked.prevUntil(prevSelector).add( $clicked ).add(prevSelector);
+
+      // Set to a range of elements
       } else {
-        $newShiftRange = $clicked.nextUntil(prevSelector).add( $clicked ).add(prevSelector);
+        var sliceMin = Math.min(this.shiftIndex, clickIndex);
+        var sliceMax = Math.max(this.shiftIndex, clickIndex);
+        var rangeIndexes;
+
+        // Select the range of elements
+        $newShiftRange  = self.$element.find(ss).slice(sliceMin, sliceMax + 1);
+        rangeIndexes    = self.getIndexes( $newShiftRange );
+
+        // If any parent-level items have only part of their
+        // children included in $newShiftRange, keep the children
+        // but remove the parent from the selection.
+        $newShiftRange.each(function() {
+          var $children = $(this).find(ss);
+
+          if($children.length && !$clicked.is(this)) {
+            // Check whether all children are included in the selection
+            var indexes = self.getIndexes( $children );
+            var containsAllChildren = indexes.every(function(val) { return rangeIndexes.indexOf(val) >= 0; });
+
+            // If not all children are included, remove parent from selection
+            if(!containsAllChildren) {
+              $newShiftRange = $newShiftRange.not( $(this) );
+            }
+          }
+        });
       }
 
+      // Select the shift range
       if(this.$shiftRange) {
-        this.selectItems( $newShiftRange.not( this.$shiftRange ) );
+        this.selectItems( $newShiftRange.not( this.$shiftRange ).find(ss).add( $newShiftRange ) );
         this.deselectItems( this.$shiftRange.not( $newShiftRange ) );
       } else {
-        this.selectItems( $newShiftRange );
-
+        this.selectItems( $newShiftRange.find(ss).add( $newShiftRange ) );
       }
 
       this.$shiftRange = $newShiftRange;
@@ -356,13 +434,11 @@
 
     // If normal click, set selection to only the clicked item
     if( !this.options.multi || (!e.ctrlKey && !e.metaKey && !e.shiftKey) ) {
-      var $deselect = this.$element.find('.' + this.options.selectedClass).not( $clicked );
-
-      if( !$clicked.hasClass(this.options.selectedClass) ) {
-        this.selectItems( $clicked );
-      }
+      var $select   = $clicked.find(ss).add( $clicked );
+      var $deselect = this.$element.find('.' + this.options.selectedClass).not( $select );
 
       this.$shiftRange = $clicked;
+      this.selectItems( $select );
       this.deselectItems( $deselect );
       this.shiftIndex = clickIndex;
     }
@@ -373,21 +449,73 @@
   };
 
   /**
+   * Set the currently active list. If hovering a nested
+   * list, that is our active list, otherwise the top
+   * level element is active.
+   *
+   * @param {object} e $.Event Object
+   */
+  Plugin.prototype.setActiveList = function(e) {
+    var self    = this;
+    var $target = $(e.currentTarget);
+    var ss      = this.options.sortSelector;
+    var sc      = this.options.selectedClass;
+
+    // TODO: '>' assumes no wrapping...not smart enough
+    function setList($list) {
+      self.$activeList  = $list;
+      self.$items       = self.$activeList.find('> ' + ss + ':not(.' + sc + ')');
+      self.setCenterPoints();
+    }
+
+    // On mouseenter just set to target
+    if(e.type === 'mouseenter') {
+      setList( $target );
+      return;
+    }
+
+    // Otherwise see if we're deeply nested and set to next highest parent,
+    // otherwise set to element.
+    var $nestedParent = $target.parents('.' + self.options.nestedMenuClass);
+
+    if($nestedParent.length > 0) {
+      setList( $nestedParent );
+    } else {
+      setList( self.$element );
+    }
+  }
+
+  /**
    * Save the centerpoint y-coordinates of each draggable item and 0 for
    * container start. Used to determine which index we're going to drop on.
    *
    * @return {undefined}
    */
-  Plugin.prototype.setCenterPoints = function() {
-    var self  = this;
-    scrollTop = self.$element.scrollTop();
+  Plugin.prototype.setCenterPoints = function(e) {
+    var self      = this;
+    var listTop   = self.$activeList.offset().top;
+    var scrollTop = self.$activeList.scrollTop();
 
+    // First center point is top of container
     self.centerPoints = [0];
 
+    // Set center point relative to active list
     self.$items.each(function(i, item) {
-      var $item = $(item);
-      self.centerPoints.push( $item.position().top + ($item.outerHeight() / 2) + scrollTop );
+      var $item   = $(item);
+      var height  = $item.outerHeight();
+      var $nested = $item.find('.' + self.options.nestedMenuClass);
+
+      // Subtract the height of every nested menu to get
+      // only the height of the item itself
+      if($nested.length > 0) {
+        $nested.each(function() {
+          height -= $(this).outerHeight();
+        });
+      }
+
+      self.centerPoints.push( ($item.offset().top - listTop) + (height / 2) + scrollTop );
     });
+
   }
 
   /**
@@ -414,7 +542,8 @@
   };
 
   /**
-   * Slide up then remove .drop_preview
+   * Slide up then remove .drop_preview and reset active menu
+   * to the top level menu
    *
    * @return {undefined}
    */
@@ -425,29 +554,41 @@
   };
 
   /**
-   * Get the indexes of everything that's selected
+   * Return an array of indexes for a $ selection
    *
-   * @return {array} Indexes of selected items
+   * @param  {object} $elements   $ Selection
+   * @return {array} Indexes      Indees of selected items
    */
-  Plugin.prototype.getSelectedIndexes = function() {
+  Plugin.prototype.getIndexes = function( $elements ) {
     var self = this;
-    var sortSelector = this.options.sortSelector;
     var indexes = [];
 
-    self.$element.addClass('test')
-
-    this.$sortSelection.each(function(i, el) {
-      indexes.push( self.$element.find(sortSelector).index(el) );
+    $elements.each(function(i, el) {
+      indexes.push( self.$element.find(self.options.sortSelector).index(el) );
     });
 
     return indexes.sort();
   }
 
   /**
+   * Wrapper for getIndexes
+   *
+   * @return #see getIndexes
+   */
+  Plugin.prototype.getSelectedIndexes = function() {
+    return this.getIndexes( this.$sortSelection );
+  }
+
+  /**
    * Set the jQuery Element that contains our sort selection
    */
   Plugin.prototype.setSortSelection = function() {
-    this.$sortSelection = this.$element.find('.'+this.options.selectedClass);
+    var self = this;
+
+    // Select only top-level elements, keep nested elements nested
+    self.$sortSelection = self.$element.find('.'+self.options.selectedClass).filter(function() {
+      return !$(this).parents('.'+self.options.selectedClass).length;
+    });
   }
 
   /**
@@ -471,6 +612,31 @@
     this.setSortSelection();
     this.options.onSelectionChange.call(this, this.getSelectedIndexes(), this.$sortSelection);
   };
+
+  /**
+   * Detect whether an event is bubbling (i.e. if it has
+   * already been handled in our plugin). This lets us
+   * stopPropgation within only our plugin.
+   *
+   * @param  {string}  eventName JS Event Name
+   * @return {Boolean}           True if event is bubbling
+   */
+  function isBubble(eventName) {
+    if(stopProp[eventName] === true) {
+      return true;
+    }
+
+    // Set the bubbling to true
+    stopProp[eventName] = true;
+
+    // Unset the bubble once we reach the window
+    $window.one(eventName, function() {
+      stopProp[eventName] = false;
+    });
+
+    // Not bubbling on first call
+    return false;
+  }
 
   // Plugin Wrapper
   $.fn[pluginName] = function ( options ) {
